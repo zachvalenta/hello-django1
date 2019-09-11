2b2d1e49a57d71743a4c4b9344ffea8c10492805
zachvalenta <zjv5002@gmail.com>
2019-09-11 11:00:29 -0400

    chapter 7 - mv index from TemplateView to function-based

--- zjvproj/urls.py
+++ zjvproj/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from zjvapp import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', views.index, name='index'),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
     path('admin/', admin.site.urls),
