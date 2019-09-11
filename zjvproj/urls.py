72750a3735a8dd915f0c63cb33d13c888f0c38f7
zachvalenta <zjv5002@gmail.com>
2019-09-10 20:34:54 -0400

    chapter 5 - template inheritance - sanity check

--- zjvproj/urls.py
+++ zjvproj/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from zjvapp import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
     path('admin/', admin.site.urls),
 ]
