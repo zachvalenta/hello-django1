7da0e78a22feaaca56a78916c286ed9a6cc5b5c6
zachvalenta <zjv5002@gmail.com>
2019-09-12 12:27:54 -0400

    add slug to admin list view display

--- zjvapp/admin.py
+++ zjvapp/admin.py
@@ -5,7 +5,7 @@ from zjvapp.models import Thing
 
 class ThingAdmin(admin.ModelAdmin):
     model = Thing
-    list_display = ('name', 'description',)  # fields for display in admin
+    list_display = ('name', 'description', 'slug')  # fields for display in admin
     prepopulated_fields = {'slug': ('name', )}  # fields to use for slug
 
 admin.site.register(Thing, ThingAdmin)
