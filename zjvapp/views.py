28bade3fc91b671b5f9fede19b126c093f61e6b4
zachvalenta <zjv5002@gmail.com>
2019-09-13 11:56:09 -0400

    rf view for more explicit control flow

--- zjvapp/views.py
+++ zjvapp/views.py
@@ -16,14 +16,11 @@ def thing_detail(request, slug):
 
 def thing_edit(request, slug):
     thing = Thing.objects.get(slug=slug)
-    form_class = ThingForm
-
+    if request.method == 'GET':
+        form = ThingForm(instance=thing)
+        return render(request, 'thing_edit.html', context={'thing':thing, 'form': form})
     if request.method == 'POST':
-        form = form_class(data=request.POST, instance=thing)
+        form = ThingForm(data=request.POST, instance=thing)
         if form.is_valid():  # necessary? paradigm? why not try/catch?
             form.save()
             return redirect('thing_detail', slug=thing.slug)
-    else:
-        form = form_class(instance=thing)
-
-    return render(request, 'thing_edit.html', context={'thing':thing, 'form': form})
