6271150e138dea7e9f86ee73ed3876c0d5868d1c
zachvalenta <zjv5002@gmail.com>
2019-09-17 08:45:28 -0400

    chapter 10 - registration - add django-registration-redux as dep, update settings, apply its migrations

--- zjvproj/settings.py
+++ zjvproj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'zjvapp',
+    'registration',  # django-registration-redux
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ACCOUNT_ACTIVATATION_DAYS = 7  # django-registration-redux 
