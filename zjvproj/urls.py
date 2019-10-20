8ca44921df0e78edd91311996e7fddca65b83499
zachvalenta <zjv5002@gmail.com>
2019-10-19 20:36:32 -0400

    urls - comments on functionality, reorder to follow user flow

--- zjvproj/urls.py
+++ zjvproj/urls.py
@@ -6,15 +6,23 @@ from django.views.generic import TemplateView
 from zjvapp import views
 
 urlpatterns = [
+    # me
     path('', views.index, name='index'),  # `home` in Osborn 1 4.13
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
     path('thing/<slug>', views.thing_detail, name='thing_detail'),
     path('thing/<slug>/edit/', views.thing_edit, name='thing_edit'),
+
+    # Registration Redux: login, logout, register form, register ack
     path('accounts/', include('registration.backends.simple.urls')),
-    path('accounts/password/done/', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name='password_reset_done'),
-    path('accounts/password/reset/<uidb64>/<token>', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name='password_reset_confirm'),
-    path('accounts/password/reset/', password_reset, {'template_name': 'registration/password_reset_form.html'}, name='password_reset'),
-    path('accounts/password/complete/', password_reset_complete, {'template_name': 'registration/password_reset_complete.html'}, name='password_reset_complete'),
+
+    # pw reset
+    path('accounts/password/reset/', password_reset, {'template_name': 'registration/password_reset_form.html'}, name='password_reset'),  # prompt email to send reset link
+    path('accounts/password/done/', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name='password_reset_done'),  # ack link sent
+    path('accounts/password/reset/<uidb64>/<token>', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name='password_reset_confirm'),  # input new pw
+    path('accounts/password/complete/', password_reset_complete, {'template_name': 'registration/password_reset_complete.html'}, name='password_reset_complete'),  # ack new
+
+    # admin
     path('admin/', admin.site.urls),
+
 ]
